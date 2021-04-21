6c45b09dd58056d20daf8f149c62924b1e2d9869
nithya5786 <82766820+nithya5786@users.noreply.github.com>
2021-04-21 21:30:52 +0530

    Update ChangeOddToUppercase.java

--- day2/ChangeOddToUppercase.java
+++ day2/ChangeOddToUppercase.java
@@ -7,22 +7,27 @@ public class ChangeOddToUppercase
 	{
 		
 		        String name = "changename";//input sting name to change odd string to upper case
-		        String output = "";//declare the output as "" to store the output
-		        for (int i = 0; i < name.length(); i++)//traverse through the length of the string 
-		        {
-		            if(i%2 == 0)//check the odd string using mod operator
-		            {
-		                output += Character.toUpperCase(name.toCharArray()[i]);//change the string to upper case and store in output
-		            }else
-		            {
-		                output += Character.toLowerCase(name.toCharArray()[i]);//Else change the string to lower case and store in output
-		            }
-		        }
-
-		        System.out.println("Newly generated string is as follows: "+ output);//print the newly generated string
-		    }
+		        char[] chars = name.toCharArray();
+                 for(int i=0;i<chars.length;i++)
+                 {
+		    
+		
+		if(i%2!=0)
+		{
+			
+			System.out.print(chars[i]);
+		}
+		
+		else
+		{
+			String output1 =Character.toString(chars[i]);
+			System.out.print(output1.toUpperCase());
+		}
+        
 		
 		}
+	}
+}
 
 	
 
