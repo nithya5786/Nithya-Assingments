546e036997f798a0d66f3835c7bb92a6b62d55b6
nithya5786 <82766820+nithya5786@users.noreply.github.com>
2021-04-23 14:41:44 +0530

    Assignment Week1 Optional and Mandatory

--- day2/ChangeOddToUppercase.java
+++ day2/ChangeOddToUppercase.java
@@ -1,38 +1,24 @@
 package week2.day2;
 
-public class ChangeOddToUppercase 
-{
+public class ChangeOddToUppercase {
 
-	public static void main(String[] args) 
-	{
-		
+	public static void main(String[] args) {
+
 		String name = "changename";// input sting name to change odd string to upper case
 		char[] chars = name.toCharArray();
-		for (int i = 0; i < chars.length; i++) 
-		{
+		for (int i = 0; i < chars.length; i++) {
 
-			if (i % 2 != 0) 
-			{
+			if (i % 2 != 0) {
 
 				
 				System.out.print(Character.toUpperCase(chars[i]));
 			}
 
-			else 
-			{
+			else {
 				System.out.print(chars[i]);
 
 			}
-
-        
-		
 		}
 	}
+
 }
-
-	
-
-
-
-
-
